ead1183214947d11a45ae4e7a206aacd45a961e8
tejas-kumar-gh <dhimantejas1234@gmail.com>
2026-03-31 10:06:24 +0530

    changes in backend

--- server/test_db_connect.js
+++ server/test_db_connect.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
         console.log(`MongoDB Connected: ${conn.connection.host}`);
         process.exit(0);
     } catch (error) {
-        console.error(`Error: ${error.message}`);
+        console.error(`ERROR: ${error.message}`);
         console.error(error);
         process.exit(1);
     }
